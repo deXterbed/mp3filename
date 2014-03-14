a57bf161e23320759cf1cd65ffdec0295d98aaa1
ph0t0n <b0rn2c0d3@gmail.com>
2014-03-14 21:00:29 +0530

    keep files in the same folder

--- lib/mp3filename.rb
+++ lib/mp3filename.rb
@@ -3,25 +3,28 @@ require 'mp3info'
 class Mp3Error < StandardError; end;
 
 class Mp3FileName
-  VERSION = "1.0.0"
+  VERSION = "1.0.3"
 
   def initialize(path)
     @path = path
   end
 
   def change
-    Dir["#{@path}/*.mp3"].each do |file|
-      Mp3Info.open(file) do |mp3|          
-        title = mp3.tag.title
-        title ||= ""        
-        file_name = title.empty? ? file.gsub(/^\d*_*/,'') : "#{title}.mp3"
+    Dir["#{@path}/*.mp3"].each do |old_file_name|
+      Mp3Info.open(old_file_name) do |mp3|          
+        new_file_name = convert_file_name(mp3, old_file_name)
         begin
-          FileUtils.mv("#{file}", file_name)
-          puts "File Name changed to : #{file_name}"
-        rescue
+          FileUtils.mv("#{old_file_name}", "#{@path}/#{new_file_name}.mp3")
+          puts "'#{old_file_name.split('/')[-1]}' changed to '#{new_file_name}.mp3'"
+        rescue          
           next
         end
       end
     end
+  end
+
+  def convert_file_name(mp3, file)
+    file_name = mp3.tag.title ? "#{mp3.tag.title}" : file.split('.mp3')[0]
+    file_name.gsub(/[\d_\-\[\]]/,'').gsub(/(\w+\.)+\w+/,'').strip
   end
 end
--- mp3filename.gemspec
+++ mp3filename.gemspec
@@ -1,12 +1,13 @@
 Gem::Specification.new do |s|
   s.name = "mp3filename"
-  s.version = "1.0.2"
+  s.version = "1.0.3"
   s.authors = ["Manoj Mishra"]
-  s.date = %q{2012-03-18}
+  s.licenses = ['MIT']
+  s.date = %q{2014-03-14}
   s.description = 'Mp3 File Name is a utility to rename mp3 files to their title'
   s.summary = s.description
   s.email = 'b0rn2c0d3@gmail.com'
-  s.files = ['README', 'lib/mp3filename.rb']
+  s.files = ['lib/mp3filename.rb']
   s.homepage = 'https://github.com/railsfreak/mp3filename'
   s.has_rdoc = false
   s.add_dependency('ruby-mp3info')
